31f7f88bdcaf583326c7d762e4f73f817841aacd
drkcrtx <dreiwolke@gmail.com>
2026-03-24 15:03:49 -0600

    Update vm.tf

--- PracticaFinal/infra/vm.tf
+++ PracticaFinal/infra/vm.tf
@@ -2,10 +2,10 @@
 resource "aws_instance" "vm" {
   ami           = var.ami
   instance_type = var.instancia
-  subnet_id = aws_subnet.subnet.id
-  vpc_id  = aws_vpc.vpc.id
+  subnet_id = var.subnet_id
   associate_public_ip_address = true
   tags = {
+    owner = "${var.tag}-vm"
     Name = "${var.region}-vm"
     
   }
